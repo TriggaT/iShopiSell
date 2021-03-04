1d39f664bd06bd35fd62b0de25f317c8d0ffe903
Trayshawn <traytwebb14@yahoo.com>
2021-03-04 12:58:14 -0600

    added fetch request for user data in rails api

--- iShopiSell-frontend/index.js
+++ iShopiSell-frontend/index.js
@@ -1,6 +1,12 @@
 let loginbtn = document.getElementById("login-button")
 let user = document.getElementById("login")
+let userBaseURL = "http://localhost:3000/users"
 
+document.addEventListener("DOMContentLoaded", function (){
+    fetch(userBaseURL)
+    .then(r => r.json())
+    .then(data => console.log(data))
+})
 
 
 
