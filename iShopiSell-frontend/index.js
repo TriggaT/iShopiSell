af44f6adf93328df3580b067f9d9f25b2fbb7ea8
Trayshawn <traytwebb14@yahoo.com>
2021-03-04 13:44:51 -0600

    added ability for current users to 'log-in'

--- iShopiSell-frontend/index.js
+++ iShopiSell-frontend/index.js
@@ -1,18 +1,24 @@
-let loginbtn = document.getElementById("login-button")
-let username = document.getElementById("login")
-let userBaseURL = "http://localhost:3000/users"
+const loginbtn = document.getElementById("login-button")
+const username = document.getElementById("login")
+const loginContainer = document.getElementById("login-container")
+const userBaseURL = "http://localhost:3000/users"
 
 document.addEventListener("DOMContentLoaded", function (){
     fetch(userBaseURL)
     .then(r => r.json())
     .then(data => { data.forEach(u => new User(u.id, u.name, u.account_balance))
-
     })
 })
 
 
 
 loginbtn.addEventListener("click", function() {
-    console.log(user.value)}
-)
+    let userNameArray = User.all.map(e => e.name.toLowerCase())
+    
+    if (userNameArray.includes(username.value.toLowerCase())){
+        loginContainer.innerHTML = ""
+    }
+    else console.log(username.value)
+
+})
 
