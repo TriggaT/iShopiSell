8964f600c652373ec681dc0b80598937ecf42715
Trayshawn <traytwebb14@yahoo.com>
2021-03-04 12:43:44 -0600

    added eventlistener to login button

---
+++ iShopiSell-frontend/index.js
@@ -0,0 +1,10 @@
+let loginbtn = document.getElementById("login-button")
+let user = document.getElementById("login")
+
+
+
+
+loginbtn.addEventListener("click", function() {
+    console.log(user.value)}
+)
+
